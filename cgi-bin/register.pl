9b81f8e06bc5400276edd6d1f36f01e09b4959ab
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2022-12-21 15:46:04 -0500

    Update v0.1

--- cgi-bin/register.pl
+++ cgi-bin/register.pl
@@ -1,20 +1,88 @@
-#!"c:/Strawberry/perl/bin/perl.exe"
+#!\xampp\perl\bin\perl
 use strict;
 use warnings;
 use CGI;
 use DBI;
 
 my $q=CGI->new;
-print $q -> header('text/xml');
+print $q->header('text/xml');
+print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+print "<report>";
+
 my $userName=$q->param("userName");
 my $password=$q->param("password");
 my $lastName=$q->param("lastName");
 my $firstName=$q->param("firstName");
 
-    
+# --- las funciones se definen primero, si no, no compila :D
+
 #register($userName,$password,$firstName,$lastName);
 
-if(defined($userName) and defined($password) and defined($firstName) and defined($lastName)){
+sub checkUserName{
+    my $UNameQuery=$_[0];
+    my $UPassQuery=$_[1];
+    my $user1 = 'root';
+    my $password1= '';
+    my $dsn ='DBI:mysql:database=wiki';
+    my $dbh = DBI ->connect($dsn,$user1,$password1) or die ("No se pudo conectar"); 
+
+    my $sql = "SELECT * FROM Users WHERE userName like '$UNameQuery' OR password like '$UPassQuery'"; 
+    my $sth=$dbh->prepare($sql);
+    $sth->execute();
+    my @row=$sth->fetchrow_array;
+    print "<content>@row</content>";
+    $sth->finish;
+    $dbh->disconnect;
+    
+    return @row;
+}
+
+sub successRegister{            
+    my $owner= $_[0];
+    my $firstNameQuery1= $_[1];
+    my $lastNameQuery1 = $_[2];
+    print <<XML;
+        <message>Success</message>
+        <user>
+            <owner>$owner</owner>
+            <firstName>$firstNameQuery1</firstName>
+            <lastName>$lastNameQuery1</lastName>
+        </user>  
+XML
+}
+
+sub showRegister{            
+    print <<XML;
+        <message>Failure</message>
+        <user>
+        </user>
+XML
+}
+
+sub register{
+
+    my $userNameQuery=$_[0];
+    my $passwordQuery=$_[1];
+    my $firstNameQuery=$_[2];
+    my $lastNameQuery=$_[3];
+
+
+    my $user1 = 'root';
+    my $password1= '';
+    my $dsn ='DBI:mysql:database=wiki';
+    my $dbh = DBI ->connect($dsn,$user1,$password1) or die ("No se pudo conectar");
+
+    my $sth=$dbh->prepare("INSERT INTO Users(userName,password,lastName,firstName) VALUES (?,?,?,?)");
+
+    #my $sql = "INSERT INTO Users(userName,password,firstName,lastName) VALUES(?,?,?,?)";
+    #my $sth=$dbh->prepare($sql);
+
+    $sth->execute($userNameQuery,$passwordQuery,$lastNameQuery,$firstNameQuery);
+    $sth ->finish;
+    $dbh->disconnect;
+}
+
+if(defined($userName) && defined($password) && defined($firstName) && defined($lastName)){
     if(!checkUserName($userName)){
         register($userName,$password,$firstName,$lastName);
         successRegister($userName,$firstName,$lastName);
@@ -27,78 +95,4 @@ else{
     showRegister();
 }
 
-  sub checkUserName{
-    my $userNameQuery=$_[0];
-    
-    my $user1 = 'alumno';
-    my $password1= 'pweb1';
-    my $dsn ='DBI:MariaDB:database=pweb1;host=192.168.1.13';
-    my $dbh = DBI ->connect($dsn,$user1,$password1) or die ("No se pudo conectar");
-    
-    
-    my $sql = "SELECT userName FROM Users WHERE userName=?";
-    my $sth=$dbh->prepare($sql);
-    $sth->execute($userNameQuery);
-    my @row=$sth->fetchrow_array;
-    $sth->finish;
-    $dbh->disconnect;
-    return @row;
-
-}
-sub successRegister{            
-    my $owner= $_[0];
-    my $firstNameQuery1= $_[1];
-    my $lastNameQuery1 = $_[2];
-    my $body=<<XML;
-    <user>
-        <owner>$owner</owner>
-        <firstName>$firstNameQuery1</firstName>
-        <lastName>$lastNameQuery1</lastName>
-    </user>
-XML
-        print(renderBody($body));
-}
-
-
-sub showRegister{            
-    my $body=<<XML;
-    <user>
-    </user>
-XML
-        print(renderBody($body));
-}
-
-sub renderBody{
-    my $body=$_[0];
-    my $xml=<<XML;
-<?xml version="1.0" encoding="UTF-8"?>
-$body
-XML
-    return $xml;
-}
-
-sub register{
-
-    my $userNameQuery=$_[0];
-    my $passwordQuery=$_[1];
-    my $firstNameQuery=$_[2];
-    my $lastNameQuery=$_[3];
-
-
-    my $user = 'alumno';
-    my $password= 'pweb1';
-    my $dsn ='DBI:MariaDB:database=pweb1;host=192.168.1.9';
-    my $dbh = DBI ->connect($dsn,$user,$password) or die ("No se pudo conectar");
-
-
-    my $sth=$dbh->prepare("INSERT INTO Users(userName,password,lastName,firstName) VALUES (?,?,?,?)");
-
-
-    #my $sql = "INSERT INTO Users(userName,password,firstName,lastName) VALUES(?,?,?,?)";
-    #my $sth=$dbh->prepare($sql);
-    $sth->execute($userNameQuery,$passwordQuery,$lastNameQuery,$firstNameQuery);
-    $sth ->finish;
-    $dbh->disconnect;
-
-
-}
+print "</report>";
--- index.pl
+++ index.pl
@@ -3,23 +3,27 @@ use strict;
 use warnings;
 use CGI;
 use DBI;
-#my $q = CGI->new;
+
+my $q = CGI->new;
 my $user1 = 'root';
 my $password1= '';
-my $dsn ='DBI:mysql:database=pweb1';
+my $dsn ='DBI:mysql:database=wiki';
 my $dbh = DBI ->connect($dsn,$user1,$password1) or die ("No se pudo conectar");
 
 #CreateEverithing
 my $Query = "";
 open(SQL,"./sql/CreateEverything.sql") or die ("No se puede abrir el query");
 while(my $line = <SQL>){
-    $Query = $Query.$line;   
-};
-#Query
-my $sth=$dbh->prepare($Query);
-$sth->execute();
-$sth ->finish;
+    $Query = $Query.$line;
+    if(index($Query,";") != -1){ 
+        my $sth=$dbh->prepare($Query);
+        $sth->execute() or die("no se puede hacer execute");
+        $sth ->finish; 
+        $Query = "";
+    }  
+}
+
 $dbh->disconnect;
-print $query;
+
 #End
-#print $q->redirect("./html/");
+print $q->redirect("./html/");
